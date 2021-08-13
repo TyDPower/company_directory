784cac2ed2c4474e80bd3a6db61f1f754a85f56d
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-08-13 03:36:01 +0100

    Create filterPersonnelByLoc.php

---
+++ libs/php/personnel/filterPersonnelByLoc.php
@@ -0,0 +1,91 @@
+<?php
+
+$executionStartTime = microtime(true);
+
+include("../common/config.php");
+
+header('Content-Type: application/json; charset=UTF-8');
+
+$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+
+if (mysqli_connect_errno()) {
+    
+    $output['status']['code'] = "300";
+    $output['status']['name'] = "failure";
+    $output['status']['description'] = "database unavailable";
+    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+    $output['data'] = [];
+    
+    mysqli_close($conn);
+
+    echo json_encode($output);
+    
+    exit;
+
+}
+
+$azArr = array('DESC', 'ASC');
+$orderArr = array('lastName', 'firstName', 'id', 'department', 'location');
+
+if (count($_POST['filter']['locations']) > 0) {
+
+    $locs = $_POST['filter']['locations'];
+    $locsCount = str_repeat('?, ', count($locs) - 1) . '?';
+
+    $az = $_POST['filter']['ascOrDsc'];
+    if (!in_array($az, $azArr)) {
+        $az = 'ASC';
+    };
+    $order = $_POST['filter']['orderBy'];
+    if (!in_array($order, $orderArr)) {
+        $order = 'lastName';
+    };
+
+    $query = null;
+
+    $query = $conn->prepare("SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location FROM personnel p LEFT JOIN department d ON (d.id = p.departmentID) LEFT JOIN location l ON (l.id = d.locationID) where l.id in ($locsCount) ORDER BY $order $az");
+
+    $types = str_repeat('i', count($locs));
+    $query->bind_param($types, ...$locs);
+    $query->execute();
+
+}
+
+
+if (false === $query) {
+
+    $output['status']['code'] = "400";
+    $output['status']['name'] = "executed";
+    $output['status']['description'] = "query failed";	
+    $output['data'] = [];
+
+    echo json_encode($output); 
+
+    mysqli_close($conn);
+    exit;
+
+}
+
+$result = $query->get_result();
+
+   $data = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+    array_push($data, $row);
+
+}
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+$output['data'] = $data;
+
+header('Content-Type: application/json; charset=UTF-8');
+
+echo json_encode($output); 
+
+mysqli_close($conn);
+
+?>

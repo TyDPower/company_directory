60c9cf45ac227258f844cfe7865c6a5330ab52f1
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-08-13 03:33:11 +0100

    Update filter.js
    Code for interacting with the filter

--- libs/js/common/filter.js
+++ libs/js/common/filter.js
@@ -1,8 +1,8 @@
 export var filter = {
-    isFiltered: true,
-    orderBy: 'firstName',
+    isFiltered: false,
+    orderBy: 'lastName',
     ascOrDsc: 'ASC',
     locations: [],
     departments: [],
     directory: null
-}
+};

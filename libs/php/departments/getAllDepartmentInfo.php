de7c3675b73252f39421a20ac4d6237bde37a7a0
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-08-13 03:35:29 +0100

    Create getAllDepartmentInfo.php

---
+++ libs/php/departments/getAllDepartmentInfo.php
@@ -0,0 +1,68 @@
+<?php
+
+	$executionStartTime = microtime(true);
+
+	include("../common/config.php");
+
+	header('Content-Type: application/json; charset=UTF-8');
+
+	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+
+	if (mysqli_connect_errno()) {
+		
+		$output['status']['code'] = "300";
+		$output['status']['name'] = "failure";
+		$output['status']['description'] = "database unavailable";
+		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+		$output['data'] = [];
+
+		mysqli_close($conn);
+
+		echo json_encode($output);
+
+		exit;
+
+	}	
+
+	$query = $conn->prepare('SELECT d.id, d.name, l.name as location, l.id as locationID, count(p.id) as personnelCount FROM department d LEFT JOIN location l ON (d.locationID = l.id) left join personnel p on (d.id = p.departmentID) where d.id = ?');
+
+	$query->bind_param("i", $_POST["id"]);
+
+	$query->execute();
+	
+	if (!$query) {
+
+		$output['status']['code'] = "400";
+		$output['status']['name'] = "executed";
+		$output['status']['description'] = "query failed";	
+		$output['data'] = [];
+
+		mysqli_close($conn);
+
+		echo json_encode($output); 
+
+		exit;
+
+	}
+
+	$result = $query->get_result();
+   
+   	$data = [];
+
+	while ($row = mysqli_fetch_assoc($result)) {
+
+		array_push($data, $row);
+
+	}
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$output['data'] = $data;
+	
+	mysqli_close($conn);
+
+	echo json_encode($output); 
+
+?>

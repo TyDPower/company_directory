6b9c453644717fd3e9f57ea5f049b026c1e715cc
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-08-13 03:34:08 +0100

    Create config.php

---
+++ libs/php/common/config.php
@@ -0,0 +1,18 @@
+<?php
+
+	//$cd_host = "db5004263839.hosting-data.io";
+	//$cd_port = 3306;
+	//$cd_socket = "";
+	//$cd_user = "dbu519700";
+	//$cd_password = "gGsOIr!dFxepWqyVAjc5EWi@pO4jgU#T";
+	//$cd_dbname = "dbs3535358";
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+	$cd_user = "root";
+	$cd_password = "rootpassword";
+	$cd_dbname = "company_directory";
+
+?>
+

7bffe174531c797d11846bd6f8a3e3a8c982f789
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-08-13 03:35:53 +0100

    Create updateLocation.php

---
+++ libs/php/locations/updateLocation.php
@@ -0,0 +1,59 @@
+<?php
+
+	$executionStartTime = microtime(true);
+
+	include("../common/config.php");
+
+	header('Content-Type: application/json; charset=UTF-8');
+
+	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+
+	if (mysqli_connect_errno()) {
+		
+		$output['status']['code'] = "300";
+		$output['status']['name'] = "failure";
+		$output['status']['description'] = "database unavailable";
+		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+		$output['data'] = [];
+
+		mysqli_close($conn);
+
+		echo json_encode($output);
+
+		exit;
+
+	}	
+
+	$loc = $_POST["obj"];
+
+	$query = $conn->prepare('UPDATE location SET name = ? WHERE id = ?');
+	
+	$query->bind_param("si", $loc["name"], $loc['id']);
+
+
+	$query->execute();
+	
+	if (false === $query) {
+
+		$output['status']['code'] = "400";
+		$output['status']['name'] = "executed";
+		$output['status']['description'] = "query failed";	
+
+		echo json_encode($output);
+
+		mysqli_close($conn); 
+
+		exit;
+
+	}
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	
+	mysqli_close($conn);
+
+	echo json_encode($output); 
+
+?>
